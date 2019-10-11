7231d3bc476216b6f4132273b8f2b2afbc52393b
desrindamala <56154030+desrindamala@users.noreply.github.com>
2019-10-11 17:07:17 +0700

    Kecepatan tanpa fungsi

--- main.py
+++ main.py
@@ -3,9 +3,29 @@ program = 'Gerak Lurus'
 
 print (f'Program {program} oleh {nama}')
 
-jarak = 1000
-waktu = 5 * 60
-kecepatan = jarak / waktu
 
-print(f'jarak= {jarak/1000}km ditempuh dalam waktu = {waktu/60}')
-print (f' Sehingga kecepatan = {kecepatan} m/s')
+
+
+def hitung_kecepatan (jarak, waktu ):
+    kecepatan = jarak / waktu
+    print(f'jarak= {jarak / 1000}km ditempuh dalam waktu = {waktu / 60}')
+    print(f' Sehingga kecepatan = {kecepatan} m/s')
+    return jarak / waktu
+
+# jarak = 1000
+# waktu = 5 * 60
+kecepatan = hitung_kecepatan (1000, 5 * 60)
+kecepatan = hitung_kecepatan (3000, 70 * 60 )
+
+
+
+def hitung_gaya (massa, waktu ):
+    kecepatan = jarak / waktu
+    print(f'jarak= {jarak / 1000}km ditempuh dalam waktu = {waktu / 60}')
+    print(f' Sehingga kecepatan = {kecepatan} m/s')
+    return jarak / waktu
+
+# jarak = 1000
+# waktu = 5 * 60
+kecepatan = hitung_kecepatan (1000, 5 * 60)
+kecepatan = hitung_kecepatan (3000, 70 * 60 )
