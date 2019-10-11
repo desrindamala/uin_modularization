b8e9c87d8d16842c3a3a35ad741b29a0d61832c5
desrindamala <56154030+desrindamala@users.noreply.github.com>
2019-10-11 17:16:33 +0700

    Kecepatan tanpa fungsi

--- main.py
+++ main.py
@@ -19,13 +19,16 @@ kecepatan = hitung_kecepatan (3000, 70 * 60 )
 
 
 
-def hitung_gaya (massa, waktu ):
-    kecepatan = jarak / waktu
-    print(f'jarak= {jarak / 1000}km ditempuh dalam waktu = {waktu / 60}')
-    print(f' Sehingga kecepatan = {kecepatan} m/s')
-    return jarak / waktu
+def hitung_gaya (massa, percepatan ):
+    gaya = massa * percepatan
+    print(f'massa= {massa / 5000}  dengan percepatan = {percepatan / 100} ')
+    print(f' Sehingga gaya = {gaya} N')
+    return massa * percepatan
 
-# jarak = 1000
-# waktu = 5 * 60
-kecepatan = hitung_kecepatan (1000, 5 * 60)
-kecepatan = hitung_kecepatan (3000, 70 * 60 )
+
+# massa = 5000
+# percepatan = 100
+gaya = hitung_gaya (5000, 100)
+gaya = hitung_gaya (2500, 40)
+
+
